9159b1f6388868b1589ac42e279a613acccd8da2
rashid-techie <70362262+rashid-techie@users.noreply.github.com>
2020-08-28 12:57:32 +0530

    Update production.js

--- production.js
+++ production.js
@@ -1,9 +1,7 @@
-//w2api - Version 0.0.1
+const fs = require('fs');
 const options = {};
 
-// options.ip = '127.0.0.1';
-// options.port = parseInt(process.argv[2]);
-// options.config = { name: 'W2API.js' };
-// options.sleep = 3000;
+//options.port = 4545;
 
-require('total.js').http('release', options);
+var framework = require('total.js');
+framework.http('release', options);
